f23d8c43688fcc39bcb24507c01e7580d0e8c789
Danilo Barros <130939835+danilosenati@users.noreply.github.com>
2026-03-01 00:02:25 -0300

    utilizando funçoes
    Separado o código em funções

--- main.py
+++ main.py
@@ -1,48 +1,66 @@
-código da etapa 1:
-
 # Projeto: Analisador Profissional de Força de Senhas
 
 # PONTUAÇÃO MÁXIMA: 9 pontos
 
-pontuacao = 0
-
-senha_usuario = input("Digite sua senha: ")
-
 # analisando criterios de segurança:
 
-if len(senha_usuario) >= 12:
-    pontuacao = 4
+def avaliar_comprimento(senha):
+    if len(senha) >= 12:
+        return 4
+        return pontuacao
+    elif len(senha) >= 8:
+        return 2
+    else:
+        return 0
 
-if len(senha_usuario) >= 8:
-    pontuacao = 2
-else:
-    pontuacao = 0
+def verificar_maiuscula(senha):
+    # caracteres maiusculos ( verifica se existe ao menos um:)
+    if any(c.isupper() for c in senha):
+        return 1
+    else:
+        return 0
 
-# caracteres maiusculos ( verifica se existe ao menos um:)
-if any(c.isupper() for c in senha_usuario):
-    pontuacao += 1
+def verificar_minuscula(senha):
+    # caracteres minusculos verifica se existe ao menos um:
+    if any(c.islower() for c in senha):
+        return 1
+    else:
+        return 0
 
-# caracteres minusculos verifica se existe ao menos um:
-if any(c.islower() for c in senha_usuario):
-    pontuacao += 1
+def verificar_numero(senha):
+    #caracteres númericos:
+    if any(c.isdigit() for c in senha):
+        return 1
+    else:
+        return 0
 
-#caracteres númericos:
-if any(c.isdigit() for c in senha_usuario):
-    pontuacao += 1
-
-# caracteres especiais ( Se não for apenas letras e números:
-if not senha_usuario.isalnum():
-    pontuacao += 2
+def verificar_especial(senha):
+    # caracteres especiais ( Se não for apenas letras e números:
+    if not senha.isalnum():
+        return 2
+    else:
+        return 0
 
 # classificação:
 
-print(f"{pontuacao} pontos")
+def classificar_pontuacao(pontos):
 
-if pontuacao <= 3:
-     print("Fraca")
-elif pontuacao >= 4 and pontuacao <= 6:
-    print("Média")
-elif pontuacao >= 7 and pontuacao <= 8:
-    print("Forte")
-else:
-    print("Muito Forte")
+    if pontos <= 3:
+        return "Fraca"
+    elif pontos >= 4 and pontos <= 6:
+        return "Média"
+    elif pontos >= 7 and pontos <= 8:
+        return "Forte"
+    else:
+        return "Muito Forte"
+
+# executando o código:
+
+senha_usuario = input("Digite sua senha: ")
+
+pontos = (avaliar_comprimento(senha_usuario) + verificar_maiuscula(senha_usuario) + verificar_minuscula(senha_usuario) + verificar_numero(senha_usuario) + verificar_especial(senha_usuario))
+
+nivel = classificar_pontuacao(pontos)
+
+print(f"Pontuação: {pontos}")
+print(f"Nivel: {nivel}")
